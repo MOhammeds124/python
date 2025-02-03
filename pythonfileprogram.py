e6c6da237090b59bb17bd982decba67c5dcaa263
MOhammeds124 <147172468+MOhammeds124@users.noreply.github.com>
2025-02-03 10:12:19 -0500

    Update pythonfileprogram.py

--- pythonfileprogram.py
+++ pythonfileprogram.py
@@ -1,36 +1,50 @@
 '''
 Mohammed sayaim
-'''
-import os
+''' 
+import os 
 from os import path
 
+msg=["new file name:","existing file name:"]
 
 def askinfo():
-    checkpoint = "askinfo"
-    whichoption = str(
-        input("1-create a new file \n"
-              "2-search for existing file \n"
-              "select option by typing 1 or 2:"))
-    checkinfo(whichoption, checkpoint)
-
-
-def checkinfo(optionwhich, pointcheck):
-    checkvalue = ord(optionwhich)
-    match (pointcheck):
+    checkpoint= "askinfo" 
+    whichoption= str(input("1-create a new file \n""2-search for existing file \n""select option by typing 1 or 2:"))
+    checkinfo(whichoption,checkpoint) 
+def checkinfo(optionwhich,pointcheck):
+    global whichfilename
+    match(pointcheck):
         case 'askinfo':
-            if (int(checkvalue) < 49 or int(checkvalue) > 50):
+            checkvalue= ord(optionwhich)
+            if(int(checkvalue)<49 or int(checkvalue)>50):
                 print("please input 1 or 2")
                 askinfo()
             else:
-                print("You have chosen wisely")
+                if (optionwhich== 1):
+                    whichfilename= str(input(msg[0]))
+                else:
+                    whichfilename= str(input(msg[1]))
+
+                whichfilename= whichfilename+".doc"
+                FileConnectivity()
                 askinfo()
-        case default:
-            print("huston... we have a problem")
+        case default: 
+            print("huzzton... we have a problem")
+            sys.exit()
+def FileConnectivity():
+    fileDir= os.path.dirname(os.path.realpath("__file__"))
+    fileexist=bool(path.exists(whichfilename))
 
+    if(fileexist==True):
+        adminfile=open(whichfilename,"r")
+    else:
+        adminfile=open(whichfilename,"x")
 
+    adminfile.close()
+        
+    
 def main():
     askinfo()
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
