dc19b2552720a035b713325ba8b8b889546d7574
MOhammeds124 <147172468+MOhammeds124@users.noreply.github.com>
2025-02-04 10:22:11 -0500

    Update pythonfileprogram.py

--- pythonfileprogram.py
+++ pythonfileprogram.py
@@ -3,38 +3,61 @@ Mohammed sayaim
 ''' 
 import os 
 from os import path
-
+import sys
+global checkpoint, whichoption
 msg=["new file name:","existing file name:"]
 
 def askinfo():
     checkpoint= "askinfo" 
-    whichoption= str(input("1-create a new file \n""2-search for existing file \n""select option by typing 1 or 2:"))
+    whichoption= str(input("1-create a new file \n""2-search for existing file \n""3- exit the program\n""select option by typing 1,2, or 3:"))
     checkinfo(whichoption,checkpoint) 
 def checkinfo(optionwhich,pointcheck):
     global whichfilename
     match(pointcheck):
         case 'askinfo':
             checkvalue= ord(optionwhich)
-            if(int(checkvalue)<49 or int(checkvalue)>50):
+            if(int(checkvalue)<49 or int(checkvalue)>51):
                 print("please input 1 or 2")
                 askinfo()
             else:
-                if (optionwhich== 1):
-                    whichfilename= str(input(msg[0]))
-                else:
-                    whichfilename= str(input(msg[1]))
+                match(optionwhich):
+                    case '1':
+                        whichfilename= str(input(msg[0]))
+                    case '2':
+                        whichfilename= str(input(msg[1]))
+                    case '3':
+                        exitprogram()
+                    case default:
+                        print("huzzton... we have a problem")
+                        sys.exit()
 
                 whichfilename= whichfilename+".doc"
                 FileConnectivity()
                 askinfo()
+        case 'exitprogram':
+            checkvalue= ord(optionwhich)
+            if(int(checkvalue)==121 or int(checkvalue)== 110):
+                if(optionwhich== "y"):
+                    sys.exit()
+                else:
+                    askinfo()
+            else:
+                print("please type y or n")
+                exitprogram()
+                
         case default: 
             print("huzzton... we have a problem")
             sys.exit()
+def exitprogram():
+    checkpoint= "exitprogram"
+    whichoption= str(input("are you sure? \n""type y for yes and n for no:"))
+    checkinfo(whichoption,checkpoint)
+    
 def FileConnectivity():
     fileDir= os.path.dirname(os.path.realpath("__file__"))
     fileexist=bool(path.exists(whichfilename))
 
-    if(fileexist==True):
+    if (fileexist==True):
         adminfile=open(whichfilename,"r")
     else:
         adminfile=open(whichfilename,"x")
